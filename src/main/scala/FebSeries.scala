a5fa5740825bdcbabc577436ea0a4f5cd8dbe843
Sai Soumith Challa <soumithsaic@gmail.com>
2019-05-29 22:00:13 -0400

    Update Functions

--- src/main/scala/FebSeries.scala
+++ src/main/scala/FebSeries.scala
@@ -1,9 +1,15 @@
-object FebSeries extends App{
+object FebSeries extends Serializable {
   def FebS(n:Int):Int={
        def febCal(n:Int,pre:Int,cur:Int):Int={
          if (n==0)pre else febCal(n-1,cur,pre+cur)
        }
        febCal(n,0,1)
   }
-  for( i <- 1  to 10)println(FebS(i))
+  def StringReverse(s:String):String={
+    return s.reverse
+  }
+
 }
+
+
+
---
+++ src/main/scala/GetFunctions.scala
@@ -0,0 +1,12 @@
+import FebSeries._
+object GetFunctions extends App {
+  val x = "Reverse"
+     for (i<-1 to 10){
+      println(FebS(i))
+}        // Getting FebSeries
+
+   println(StringReverse(x))
+
+
+
+}

7954fb72d4271fc251d61bd751ca4cc23b1cdd45
Sai Soumith Challa <soumithsaic@gmail.com>
2019-06-02 19:57:25 -0400

    Flask Templetes

--- Flask/app.py
+++ Flask/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask ,render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-        return "Testing Flask"
+        return render_template('index.html')
+@app.route('/about')
+def about():
+       return render_template('about.html')
 if __name__ == '__main__':
         app.run(debug=True,port=5001)   #Changing Debug mode without Restarting Server  and changing port number as well
+
+
+
+
 
 
 

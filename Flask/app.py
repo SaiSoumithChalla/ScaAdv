cdc25190d06b0e3e39ba0cf67a78d7d850d5af07
Sai Soumith Challa <soumithsaic@gmail.com>
2019-06-02 22:46:41 -0400

    Adding modules url_for and redirect and render_templete

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask ,render_template
+from flask import Flask ,render_template,url_for,redirect
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -6,6 +6,9 @@ def index():
 @app.route('/about')
 def about():
        return render_template('about.html')
+@app.route('/urlfor')
+def urlfor():
+      return redirect(url_for('about'))   # Redirecting to one endpoint to another end point
 if __name__ == '__main__':
         app.run(debug=True,port=5001)   #Changing Debug mode without Restarting Server  and changing port number as well
 

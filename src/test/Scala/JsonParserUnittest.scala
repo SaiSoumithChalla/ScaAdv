4093c7e483fa995bb0cf573698ca133697123c55
SoumithChalla <51181645+SoumithChala@users.noreply.github.com>
2019-07-23 10:04:37 -0400

    Update and rename src/JsonParserUnittest.scala to src/test/Scala/JsonParserUnittest.scala

--- src/test/Scala/JsonParserUnittest.scala
+++ src/test/Scala/JsonParserUnittest.scala
@@ -17,7 +17,7 @@ class JsonParserUnitTest extends FlatSpec {
 
   "Input String" should "Split Valid Json " in {
     val Source1 = InpStg
-    val RealJs =s""" {"vistorId" : "v1","products": [{"\id\": "i1",\"interest\": 0.68}, {"\id\": "i2",\"interest\": 0.42}]}"""
+    val RealJs =s""" {"visitorId" : "v1","products": [{"\id\": "i1",\"interest\": 0.68}, {"\id\": "i2",\"interest\": 0.42}]}"""
     println(Source1)
     println(RealJs)
     assert(RealJs === Source1)

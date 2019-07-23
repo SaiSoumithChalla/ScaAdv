2fe4506862f63e7d2ffbe11052c44e0c142cec81
SoumithChalla <51181645+SoumithChala@users.noreply.github.com>
2019-07-23 09:58:11 -0400

    Create JsonParserUnittest.scala

---
+++ src/JsonParserUnittest.scala
@@ -0,0 +1,26 @@
+import org.scalatest.{FlatSpec, Matchers}
+
+
+class JsonParserUnitTest extends FlatSpec {
+
+
+    val InpStg  : String =
+      """{
+    "visitorId": "v1",
+    "products": [{
+         "id": "i1",
+         "interest": 0.68
+    }, {
+         "id": "i2",
+         "interest": 0.42
+    }]}""".stripMargin
+
+  "Input String" should "Split Valid Json " in {
+    val Source1 = InpStg
+    val RealJs =s""" {"vistorId" : "v1","products": [{"\id\": "i1",\"interest\": 0.68}, {"\id\": "i2",\"interest\": 0.42}]}"""
+    println(Source1)
+    println(RealJs)
+    assert(RealJs === Source1)
+  }
+
+}
